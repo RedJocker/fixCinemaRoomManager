d0b9ec7f37bdf550253c59cfa13bc82d6c3be1a6
RedJocker85 <mauricio.guaruja@gmail.com>
2022-03-16 00:21:45 -0300

    stage5 test initial-fix

--- Cinema Room Manager/stage5/src/test/java/org/hyperskill/cinema/Stage5UnitTest.kt
+++ Cinema Room Manager/stage5/src/test/java/org/hyperskill/cinema/Stage5UnitTest.kt
@@ -1,25 +1,47 @@
 package org.hyperskill.cinema
 
+import android.app.AlertDialog
+import android.widget.GridLayout
 import android.widget.TextView
+import androidx.core.view.forEachIndexed
 import org.hyperskill.cinema.abstraction.AbstractUnitTest
+import org.hyperskill.cinema.abstraction.find
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
-// Version 21.11.2021
+// Version 03.2022
 @RunWith(RobolectricTestRunner::class)
 class Stage5UnitTest : AbstractUnitTest<MainActivity>(MainActivity::class.java) {
+
+
+    private val `for total income view` by lazy {
+        activity.find<TextView>("cinema_room_total_income")
+    }
+
+    private val `for current income view` by lazy {
+        activity.find<TextView>("cinema_room_current_income")
+    }
+
+    private val `for available seats view` by lazy {
+        activity.find<TextView>("cinema_room_available_seats")
+    }
+
+    private val `for occupied seats view` by lazy {
+        activity.find<TextView>("cinema_room_occupied_seats")
+    }
 
     @Test
     fun `test should check total cinema income value for most profitable movie`() {
         val message = "have you calculated total income properly?"
 
         activityController.`launch this activity and execute`(arguments = `most profitable movie`()) {
-            `for total income view`().`text should be`(message) { text ->
+            `for total income view`.`text should`(assertMessage = message) { text ->
                 val startsWith = text.startsWith("Total cinema income: ")
                 val endsWith = text.endsWith("\$")
                 val containsDouble = text.`is contain double`(964.29, 0.1)
-                return@`text should be` startsWith && endsWith && containsDouble
+
+                return@`text should` startsWith && endsWith && containsDouble
             }
         }
     }
@@ -29,11 +51,11 @@ class Stage5UnitTest : AbstractUnitTest<MainActivity>(MainActivity::class.java) 
         val message = "have you calculated total income properly?"
 
         activityController.`launch this activity and execute`(arguments = `default profitable movie`()) {
-            `for total income view`().`text should be`(message) { text ->
+            `for total income view`.`text should`(assertMessage = message) { text ->
                 val startsWith = text.startsWith("Total cinema income: ")
                 val endsWith = text.endsWith("\$")
-                val containsDouble = text.`is contain double`(853.39, 0.1)
-                return@`text should be` startsWith && endsWith && containsDouble
+                val containsDouble = text.`is contain double`(850.50, 0.1)
+                return@`text should` startsWith && endsWith && containsDouble
             }
         }
     }
@@ -43,11 +65,11 @@ class Stage5UnitTest : AbstractUnitTest<MainActivity>(MainActivity::class.java) 
         val message = "have you calculated current income properly?"
 
         activityController.`launch this activity and execute`(arguments = `most profitable movie`()) {
-            `for current income view`().`text should be`(message) { text ->
-                val startsWith = text.startsWith("Current cinema income: ")
+            `for current income view`.`text should`(assertMessage = message) { text ->
+                val startsWith = text.startsWith("Current cinema income: ", ignoreCase = true)
                 val endsWith = text.endsWith("\$")
                 val containsDouble = text.`is contain double`(0.0, 0.1)
-                return@`text should be` startsWith && endsWith && containsDouble
+                return@`text should` startsWith && endsWith && containsDouble
             }
         }
     }
@@ -57,11 +79,11 @@ class Stage5UnitTest : AbstractUnitTest<MainActivity>(MainActivity::class.java) 
         val message = "have you calculated current income properly?"
 
         activityController.`launch this activity and execute`(arguments = `default profitable movie`()) {
-            `for current income view`().`text should be`(message) { text ->
-                val startsWith = text.startsWith("Current cinema income: ")
+            `for current income view`.`text should`(assertMessage =  message) { text ->
+                val startsWith = text.startsWith("Current cinema income: ", ignoreCase = true)
                 val endsWith = text.endsWith("\$")
                 val containsDouble = text.`is contain double`(0.0, 0.1)
-                return@`text should be` startsWith && endsWith && containsDouble
+                return@`text should` startsWith && endsWith && containsDouble
             }
         }
     }
@@ -73,11 +95,11 @@ class Stage5UnitTest : AbstractUnitTest<MainActivity>(MainActivity::class.java) 
         activityController.`launch this activity and execute`(arguments = `most profitable movie`()) {
             `grid layout child`(index = 5).`perform click`()
             `in alert dialog`().`for positive button`().`perform click`()
-            `for current income view`().`text should be`(message) { text ->
-                val startsWith = text.startsWith("Current cinema income: ")
+            `for current income view`.`text should`(assertMessage = message) { text ->
+                val startsWith = text.startsWith("Current cinema income: ", ignoreCase = true)
                 val endsWith = text.endsWith("\$")
                 val containsDouble = text.`is contain double`(24.11, 0.1)
-                return@`text should be` startsWith && endsWith && containsDouble
+                return@`text should` startsWith && endsWith && containsDouble
             }
         }
     }
@@ -89,11 +111,11 @@ class Stage5UnitTest : AbstractUnitTest<MainActivity>(MainActivity::class.java) 
         activityController.`launch this activity and execute`(arguments = `default profitable movie`()) {
             `grid layout child`(index = 5).`perform click`()
             `in alert dialog`().`for positive button`().`perform click`()
-            `for current income view`().`text should be`(message) { text ->
-                val startsWith = text.startsWith("Current cinema income: ")
+            `for current income view`.`text should`(assertMessage = message) { text ->
+                val startsWith = text.startsWith("Current cinema income: ", ignoreCase = true)
                 val endsWith = text.endsWith("\$")
-                val containsDouble = text.`is contain double`(21.33, 0.1)
-                return@`text should be` startsWith && endsWith && containsDouble
+                val containsDouble = text.`is contain double`(21.26, 0.1)
+                return@`text should` startsWith && endsWith && containsDouble
             }
         }
     }
@@ -103,10 +125,10 @@ class Stage5UnitTest : AbstractUnitTest<MainActivity>(MainActivity::class.java) 
         val message = "have you really counted available seats?"
 
         activityController.`launch this activity and execute`(arguments = `most profitable movie`()) {
-            `for available seats view`().`text should be`(message) { text ->
+            `for available seats view`.`text should`(assertMessage = message) { text ->
                 val startsWith = text.startsWith("Available seats: ")
                 val containsInteger = text.`is contain integer`(56)
-                return@`text should be` startsWith && containsInteger
+                return@`text should` startsWith && containsInteger
             }
         }
     }
@@ -118,10 +140,12 @@ class Stage5UnitTest : AbstractUnitTest<MainActivity>(MainActivity::class.java) 
         activityController.`launch this activity and execute`(arguments = `most profitable movie`()) {
             `grid layout child`(index = 5).`perform click`()
             `in alert dialog`().`for positive button`().`perform click`()
-            `for available seats view`().`text should be`(message) { text ->
-                val startsWith = text.startsWith("Available seats: ")
-                val containsInteger = text.`is contain integer`(55)
-                return@`text should be` startsWith && containsInteger
+            `for available seats view`.`text should`(assertMessage = message) { text ->
+                val startsWithExpectedText = text.startsWith("Available seats: ", ignoreCase = true)
+                val containsExpectedInteger = text.`is contain integer`(55)
+                println(text)
+
+                return@`text should` startsWithExpectedText && containsExpectedInteger
             }
         }
     }
@@ -131,10 +155,10 @@ class Stage5UnitTest : AbstractUnitTest<MainActivity>(MainActivity::class.java) 
         val message = "have you really counted occupied seats?"
 
         activityController.`launch this activity and execute`(arguments = `most profitable movie`()) {
-            `for occupied seats view`().`text should be`(message) { text ->
-                val startsWith = text.startsWith("Occupied seats: ")
+            `for occupied seats view`.`text should`(assertMessage = message) { text ->
+                val startsWith = text.startsWith("Occupied seats: ", ignoreCase = true)
                 val containsInteger = text.`is contain integer`(0)
-                return@`text should be` startsWith && containsInteger
+                return@`text should` startsWith && containsInteger
             }
         }
     }
@@ -146,20 +170,102 @@ class Stage5UnitTest : AbstractUnitTest<MainActivity>(MainActivity::class.java) 
         activityController.`launch this activity and execute`(arguments = `most profitable movie`()) {
             `grid layout child`(index = 5).`perform click`()
             `in alert dialog`().`for positive button`().`perform click`()
-            `for occupied seats view`().`text should be`(message) { text ->
-                val startsWith = text.startsWith("Occupied seats: ")
+            `for occupied seats view`.`text should`(assertMessage = message) { text ->
+                val startsWith = text.startsWith("Occupied seats: ", ignoreCase = true)
                 val containsInteger = text.`is contain integer`(1)
-                return@`text should be` startsWith && containsInteger
+                return@`text should` startsWith && containsInteger
             }
         }
     }
 
-    private fun MainActivity.`for total income view`() = find<TextView>("cinema_room_total_income")
+    @Test
+    fun `when seat is clicked twice only first click should count`() {
+        val message = "When seat is clicked twice only first click should count"
 
-    private fun MainActivity.`for current income view`() = find<TextView>("cinema_room_current_income")
+        lateinit var lastAlertDialog: AlertDialog
 
-    private fun MainActivity.`for available seats view`() = find<TextView>("cinema_room_available_seats")
+        activityController.`launch this activity and execute`(arguments = `most profitable movie`()) {
+            repeat(2) {
+                `grid layout child`(index = 10).`perform click`()
 
-    private fun MainActivity.`for occupied seats view`() = find<TextView>("cinema_room_occupied_seats")
+                if(it == 0) {
+                    lastAlertDialog = `in alert dialog`()
+                    lastAlertDialog.`for positive button`().`perform click`()
+                } else {
+                    val currentAlertDialog = `in alert dialog`()
+                    currentAlertDialog.`should be same as`(lastAlertDialog)
+                }
 
+                `for occupied seats view`.`text should`(assertMessage = message) { text ->
+                    val startsWith = text.startsWith("Occupied seats: ", ignoreCase = true)
+                    val containsInteger = text.`is contain integer`(1)
+                    return@`text should` startsWith && containsInteger
+                }
+                `for available seats view`.`text should`(assertMessage = message) { text ->
+                    val startsWith = text.startsWith("Available seats: ", ignoreCase = true)
+                    val containsInteger = text.`is contain integer`(55)
+                    return@`text should` startsWith && containsInteger
+                }
+                `for current income view`.`text should`(assertMessage = message) { text ->
+                    val startsWith = text.startsWith("Current cinema income: ", ignoreCase = true)
+                    val endsWith = text.endsWith("\$")
+                    val containsDouble = text.`is contain double`(21.81, 0.1)
+                    return@`text should` startsWith && endsWith && containsDouble
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `when all seats clicked final state should be displayed`() {
+        val clickFailedToOpenNewDialog = "Failed to open new dialog after click on available seat"
+        val occupiedSeatCounterError = "Occupied Seats displayed wrong number"
+        val availableSeatsCounterError = "Available Seats displayed wrong number"
+        val totalIncomeCounterError = "Total Income displayed wrong number"
+
+        var lastAlertDialog: AlertDialog? = null
+
+        val expectedRowValues = listOf(
+            24.11, 21.81, 19.52,
+            17.22, 14.92, 12.63, 10.33
+        ) // these values are rounded, but error margin should be enough to compensate comparing against values with higher precision
+
+        var accumulatedIncome = 0.0
+
+        activityController.`launch this activity and execute`(arguments = `most profitable movie`()) {
+            val placesGrid = find<GridLayout>("cinema_room_places")
+            placesGrid.forEachIndexed { seatIndex, seatCard ->
+                seatCard.`perform click`()
+                val currentAlertDialog = `in alert dialog`()
+
+                lastAlertDialog?.also { lastDialog ->
+                    currentAlertDialog.`shouldn't be same as`(clickFailedToOpenNewDialog, lastDialog)
+                }
+
+                lastAlertDialog = currentAlertDialog
+                currentAlertDialog.`for positive button`().`perform click`()
+
+                `for occupied seats view`.`text should`(assertMessage = occupiedSeatCounterError) { expectedText ->
+                    val startsWith = expectedText.startsWith("Occupied seats: ", ignoreCase = true)
+                    val containsInteger = expectedText.`is contain integer`(1 + seatIndex)
+                    return@`text should` startsWith && containsInteger
+                }
+
+                `for available seats view`.`text should`(assertMessage = availableSeatsCounterError) { expectedText ->
+                    val startsWith = expectedText.startsWith("Available seats: ", ignoreCase = true)
+                    val containsInteger = expectedText.`is contain integer`(56  - (seatIndex + 1))
+                    return@`text should` startsWith && containsInteger
+                }
+
+                accumulatedIncome += expectedRowValues[seatIndex / 8]
+
+                `for current income view`.`text should`(assertMessage = totalIncomeCounterError) { expectedText ->
+                    val startsWith = expectedText.startsWith("Current cinema income: ", ignoreCase = true)
+                    val endsWith = expectedText.endsWith("\$")
+                    val containsDouble = expectedText.`is contain double`(accumulatedIncome, 0.1)
+                    return@`text should` startsWith && endsWith && containsDouble
+                }
+            }
+        }
+    }
 }
---
+++ Cinema Room Manager/stage5/src/test/java/org/hyperskill/cinema/abstraction/AbstractUnitTest.kt
@@ -0,0 +1,157 @@
+package org.hyperskill.cinema.abstraction
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Intent
+import android.os.Looper
+import android.view.View
+import android.widget.Button
+import android.widget.GridLayout
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import org.hyperskill.cinema.MainActivity
+import org.junit.Assert
+import org.junit.Assert.assertNotNull
+import org.robolectric.Shadows
+import org.robolectric.android.controller.ActivityController
+import org.robolectric.shadows.ShadowAlertDialog
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+import kotlin.math.abs
+
+// Version 03.2022
+abstract class AbstractUnitTest<T : Activity>(
+    activityClass: Class<T>,
+) : ActivityUnitTest<T>(activityClass) {
+
+
+    protected fun `most profitable movie`() = Intent().apply {
+        putExtra("DURATION", 90)
+        putExtra("RATING", 5.0f)
+    }
+
+    protected fun `default profitable movie`() = Intent().apply {
+        putExtra("DURATION", 108)
+        putExtra("RATING", 4.5f)
+    }
+
+    protected inline fun <reified A : Activity> ActivityController<A>.`launch this activity and execute`(
+        arguments: Intent? = Intent(),
+        crossinline action: A.() -> Unit
+    ): ActivityController<A> {
+        get().intent = arguments
+        return setup().also { it.get().apply(action) }
+    }
+
+    protected fun MainActivity.`grid layout child`(index: Int): View {
+        return find<GridLayout>("cinema_room_places").getChildAt(index)
+    }
+
+    protected fun MainActivity.`grid layout child`(index: (GridLayout) -> Int): View {
+        val gridLayout = find<GridLayout>("cinema_room_places")
+        return gridLayout.getChildAt(index(gridLayout))
+    }
+
+    protected fun View.indicator(): CardView {
+        return find("cinema_room_place_indicator")
+    }
+
+    protected fun CardView.`color should be`(assertMessage: String, color: Int) {
+        Assert.assertEquals(assertMessage, cardBackgroundColor.defaultColor, color)
+    }
+
+    protected fun CardView.`color shouldn't be`(assertMessage: String, color: Int) {
+        Assert.assertNotEquals(assertMessage, cardBackgroundColor.defaultColor, color)
+    }
+
+    protected fun CardView.`color shouldn't be`(color: Int, assertMessage: (CardView) -> String) {
+        Assert.assertNotEquals(assertMessage(this), cardBackgroundColor.defaultColor, color)
+    }
+
+    protected fun View.`perform click`() {
+        performClick()
+        Shadows.shadowOf(Looper.getMainLooper()).runToEndOfTasks()
+    }
+
+    protected fun `in alert dialog`(): AlertDialog {
+        val message = "The dialog wasn't created previously or you import an androidx version instead of android"
+
+        val alertDialog = ShadowAlertDialog.getLatestAlertDialog()
+        assertNotNull(message, alertDialog)
+        return alertDialog
+    }
+
+    protected fun AlertDialog.`for dialog title`(): TextView = find("alertTitle", "android")
+
+    protected fun AlertDialog.`for dialog message`(): TextView = find("message", "android")
+
+    protected fun AlertDialog.`for positive button`(): Button = getButton(Dialog.BUTTON_POSITIVE)
+
+    protected fun AlertDialog.`for negative button`(): Button = getButton(Dialog.BUTTON_NEGATIVE)
+
+    protected infix fun AlertDialog.`should be same as`(dialog: AlertDialog) {
+        Assert.assertEquals(dialog, this)
+    }
+
+    protected fun AlertDialog.`should be same as`(assertMessage: String, dialog: AlertDialog) {
+        Assert.assertEquals(assertMessage, dialog, this)
+    }
+
+    protected fun AlertDialog.`shouldn't be same as`(assertMessage: String, dialog: AlertDialog) {
+        Assert.assertNotEquals(assertMessage, dialog, this)
+    }
+
+    protected fun AlertDialog.`shouldn't be same as`(
+        dialog: AlertDialog,
+        assertMessage: (AlertDialog) -> String
+    ) {
+        Assert.assertNotEquals(assertMessage(this), dialog, this)
+    }
+
+    protected infix fun TextView.`text should be`(string: String) {
+        val actual = text.toString().lowercase()
+        val expected = string.lowercase()
+        Assert.assertEquals("Expected a $string text in $this", expected, actual)
+    }
+
+    protected fun TextView.`text should`(assertMessage: String, action: (String) -> Boolean) {
+        println(text.toString())
+        Assert.assertTrue(assertMessage, action(text.toString()))
+    }
+
+    protected fun TextView.`text should contain double`(assertMessage: String, value: Double, `with delta`: Double) {
+        println("actual: $text |expected: $value")
+        Assert.assertTrue(assertMessage, text.toString().`is contain double`(value, `with delta`))
+    }
+
+    protected fun TextView.`text should be`(errorMessage: String, expected: String) {
+        println(text.toString())
+        Assert.assertEquals(errorMessage, expected, text)
+    }
+
+    protected infix fun View.`visibility should be`(visibility: Int) {
+        Assert.assertEquals(visibility, this.visibility)
+    }
+
+    protected fun String.`is contain double`(expected: Double, `with delta`: Double): Boolean {
+
+        val matcher: Matcher = Pattern.compile("[0-9]+(\\.[0-9]+)?").matcher(this)
+        while (matcher.find()) {
+            val scanned = matcher.group().toDoubleOrNull() ?: continue
+            println("actual: $this |expected: $expected| diff: ${abs(scanned - expected)}")
+            if (abs(expected - scanned) < `with delta`) return true
+        }
+        return false
+    }
+
+    protected fun String.`is contain integer`(expected: Int): Boolean {
+        println("actual: $this |expected: $expected")
+        val matcher: Matcher = Pattern.compile("[0-9]+").matcher(this)
+        while (matcher.find()) {
+            val scanned = matcher.group().toIntOrNull() ?: continue
+            if (expected == scanned) return true
+        }
+        return false
+    }
+}
